717024bb3edf9cfe10dc8a5d4ab58cf22f4b96e2
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-11-28 14:33:43 +0100

    xxx

---
+++ gapps.mk
@@ -0,0 +1,10 @@
+# Stock GApps from device dump
+PRODUCT_PACKAGES += \
+    GoogleServicesFramework \
+    GmsCore \
+    Phonesky \
+    CalendarGoogle \
+    GoogleContacts
+
+# GApps configuration
+PRODUCT_GMS_CLIENTID_BASE := android-google
